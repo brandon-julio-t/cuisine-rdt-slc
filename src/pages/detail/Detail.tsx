6fea7e3b24bf7279df2c049dfbae6c1dd0800704
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-10-20 09:12:27 +0700

    feat: model loading indicator

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -23,14 +23,11 @@ import VideoModal from './components/VideoModal';
 const Detail: FunctionComponent = () => {
   const { id } = useParams<'id'>();
 
-  if (!id)
-    return <h1 className='text-4xl font-bold text-center'>Invalid ID.</h1>;
+  if (!id) return <h1 className="text-4xl font-bold text-center">Invalid ID.</h1>;
 
   const canvas = useRef<HTMLCanvasElement | null>(null);
-  const [food, foodModel] = useFoodModel(id);
-  const [canvasSystem, setCanvasSystem] = useState<FoodOrbitCanvas | null>(
-    null,
-  );
+  const { food, model: foodModel, isLoading } = useFoodModel(id);
+  const [canvasSystem, setCanvasSystem] = useState<FoodOrbitCanvas | null>(null);
   const [showVideoModal, setShowVideoModal] = useState(false);
   const [showRecipeModal, setShowRecipeModal] = useState(false);
   const [showIngredientsModal, setShowIngredientsModal] = useState(false);
@@ -53,65 +50,68 @@ const Detail: FunctionComponent = () => {
       id: 1,
       title: 'Recipe',
       callback: setShowRecipeModal,
-      iconElm: <QuestionMarkCircleIcon className='h-4 w-4 cursor-pointer' />,
+      iconElm: <QuestionMarkCircleIcon className="h-4 w-4 cursor-pointer" />,
     },
     {
       id: 2,
       title: 'Ingredients',
       callback: setShowIngredientsModal,
-      iconElm: <InformationCircleIcon className='h-4 w-4 cursor-pointer' />,
+      iconElm: <InformationCircleIcon className="h-4 w-4 cursor-pointer" />,
     },
     {
       id: 3,
       title: 'Video',
       callback: setShowVideoModal,
-      iconElm: <PlayIcon className='h-4 w-4 cursor-pointer' />,
+      iconElm: <PlayIcon className="h-4 w-4 cursor-pointer" />,
     },
   ];
 
   return (
     <>
-      <div className='absolute top-4 w-full z-20'>
-        <div className='relative'>
+      <div className="absolute top-4 w-full z-20">
+        <div className="relative">
           {/* Top Left */}
-          <Link className='absolute top-3 left-4 z-30' to='/'>
-            <div className='rounded-full border shadow hover:shadow-md p-2 bg-white/80 backdrop-blur max-w-min'>
-              <ChevronLeftIcon className='h-5 w-5' />
+          <Link className="absolute top-3 left-4 z-30" to="/">
+            <div className="rounded-full border shadow hover:shadow-md p-2 bg-white/80 backdrop-blur max-w-min">
+              <ChevronLeftIcon className="h-5 w-5" />
             </div>
           </Link>
 
           {/* Top Middle */}
           <div>
-            <Card className='w-52 md:w-72 lg:w-96 md:min-w-xl mx-auto bg-white/80 backdrop-blur'>
-              <h1 className='text-md sm:text-lg md:text-xl lg:text-2xl font-semibold text-center'>
+            <Card className="w-52 md:w-72 lg:w-96 md:min-w-xl mx-auto bg-white/80 backdrop-blur">
+              <h1 className="text-md sm:text-lg md:text-xl lg:text-2xl font-semibold text-center">
                 {food ? food.name : <Skeleton />}
               </h1>
+              {isLoading && <small className="block text-center">(Model loading...)</small>}
             </Card>
           </div>
 
           {/* Top Right */}
-          <div className='absolute top-3 right-4'>
-            <div className='cursor-pointer relative rounded-full border shadow hover:shadow-md p-2 bg-white/80 max-w-min'>
+          <div className="absolute top-3 right-4">
+            <div className="cursor-pointer relative rounded-full border shadow hover:shadow-md p-2 bg-white/80 max-w-min">
               <MenuIcon
                 onClick={() => {
                   setShowPopupMenu(!showPopupMenu);
                 }}
-                className='h-5 w-5'
+                className="h-5 w-5"
               />
 
               <ul
                 className={`${
                   showPopupMenu ? 'opacity-100' : 'opacity-0'
-                } transition-all duration-300 p-1 absolute divide-y-2 bg-white shadow-lg border border-gray-200 rounded top-10 right-8`}>
-                {buttons.map((btn) => (
+                } transition-all duration-300 p-1 absolute divide-y-2 bg-white shadow-lg border border-gray-200 rounded top-10 right-8`}
+              >
+                {buttons.map(btn => (
                   <li key={btn.id}>
                     <button
                       onClick={() => {
                         btn.callback(true);
                       }}
-                      className='flex py-2 pl-2 pr-8 items-center'>
+                      className="flex py-2 pl-2 pr-8 items-center"
+                    >
                       {btn.iconElm}
-                      <span className='ml-1'>{btn.title}</span>
+                      <span className="ml-1">{btn.title}</span>
                     </button>
                   </li>
                 ))}
@@ -122,37 +122,28 @@ const Detail: FunctionComponent = () => {
       </div>
 
       {/* Bottom Middle */}
-      <div className='w-full absolute bottom-4 z-20 flex justify-center'>
-        <DetailPopUp food={food} className='flex-1' />
+      <div className="w-full absolute bottom-4 z-20 flex justify-center">
+        <DetailPopUp food={food} className="flex-1" />
       </div>
 
-      <canvas ref={canvas} className='absolute inset-0'></canvas>
+      <canvas ref={canvas} className="absolute inset-0"></canvas>
 
-      <VideoModal
-        show={showVideoModal}
-        onClose={() => setShowVideoModal(false)}
-        food={food}
-      />
-      <RecipeModal
-        show={showRecipeModal}
-        onClose={() => setShowRecipeModal(false)}
-        food={food}
-      />
-      <IngredientsModal
-        show={showIngredientsModal}
-        onClose={() => setShowIngredientsModal(false)}
-        food={food}
-      />
+      <VideoModal show={showVideoModal} onClose={() => setShowVideoModal(false)} food={food} />
+      <RecipeModal show={showRecipeModal} onClose={() => setShowRecipeModal(false)} food={food} />
+      <IngredientsModal show={showIngredientsModal} onClose={() => setShowIngredientsModal(false)} food={food} />
     </>
   );
 };
 
-const useFoodModel = (id: string): [Food | null, Group | null] => {
+const useFoodModel = (id: string) => {
+  const [isLoading, setIsLoading] = useState(false);
   const [food, setFood] = useState<Food | null>(null);
   const [model, setModel] = useState<Group | null>(null);
 
   useEffect(() => {
     (async () => {
+      setIsLoading(true);
+
       const food = await FoodService.getOneById(id);
       if (!food) return;
       setFood(food);
@@ -161,10 +152,12 @@ const useFoodModel = (id: string): [Food | null, Group | null] => {
       const model = await loader.loadAsync(food.modelUrl);
       const foodModel = model.scene;
       setModel(foodModel);
+
+      setIsLoading(false);
     })();
   }, [id]);
 
-  return [food, model];
+  return { food, model, isLoading };
 };
 
 export default Detail;
